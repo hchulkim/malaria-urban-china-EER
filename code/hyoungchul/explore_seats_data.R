75838a827954273906a433616f7a1b52bc3fab00
Hyoungchul Kim <hchul.kim96@gmail.com>
2025-11-13 14:49:14 -0500

    create number of grid cells figure.

--- code/hyoungchul/explore_seats_data.R
+++ code/hyoungchul/explore_seats_data.R
@@ -5,17 +5,15 @@ pacman::p_load(here, data.table, dplyr, ggplot2, fixest, texreg, kableExtra, bro
 # load the panel data
 panel_data <- haven::read_dta(here("data", "raw", "PanelSouthChina.dta")) |> as.data.table()
 
-
-# plot histogram
-ggplot(panel_data, aes(x = Seats)) +
+# create ggplot of number of 0.5 grids within a 2x2 degree grid
+panel_data[, .(n = .N), by = .(Grid2, year)] |> 
+#histogram of n
+  ggplot(aes(x = n, fill = as.factor(year))) +
   geom_histogram(binwidth = 1) +
-  labs(x = "Seats", y = "Count") +
+  scale_x_continuous(breaks = seq(0, 16, by = 1)) +
+  scale_fill_discrete(name = "Year") +
+  labs(x = "Number of 0.5 degree grids within a 2x2 degree grid", y = "Count", title = "Histogram of number of 0.5 degree grids within a 2x2 degree grid") +
   theme_bw(base_size = 12)
 
 # save the plot
-ggsave(here("output", "figures", "author_appendix_fig_a1.png"), width = 10, height = 6)
-
-
-
-# plot histogram of Seats by year
-seat_dup <- panel_data[year == -200][, .(n = .N), by = .(Seats)][order(Seats)]
+ggsave(here("output", "figures", "grid_count_histogram.png"), width = 10, height = 6)
